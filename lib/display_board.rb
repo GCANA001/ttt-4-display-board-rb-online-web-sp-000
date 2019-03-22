10d6ef31885b39195766120ee77dd7c9e40851f8
Glenn Canavan <gcana001@fiu.edu>
2019-03-22 01:14:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,3 @@
 def display_board(board)
-  puts "0: #{board[0]}, 1: #{board[1]}, 2: #{board[2]}, 3: #{board[3]}, 4: #{board[4]}, 5: #{board[5]}, 6: #{board[6]}, 7: #{board[6]}, 8: #{board[6]}, 9: #{board[6]}"
+board = " X | X | X "
 end
