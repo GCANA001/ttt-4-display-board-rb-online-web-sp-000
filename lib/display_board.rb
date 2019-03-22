acf371bc3d62ec83c39ab1abe8931334da0897db
Glenn Canavan <gcana001@fiu.edu>
2019-03-22 01:13:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,3 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts "0: #{board[0]}, 1: #{board[1]}, 2: #{board[2]}, 3: #{board[3]}, 4: #{board[4]}, 5: #{board[5]}, 6: #{board[6]}, 7: #{board[6]}, 8: #{board[6]}, 9: #{board[6]}"
+end
